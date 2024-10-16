0119325d7bb91584c8d7a76fadb3e4da802ac5ed
Kinho12 <erichunter1412@gmail.com>
2024-10-16 08:42:49 -0300

    realizando mod8

---
+++ Tarefa_media.java
@@ -0,0 +1,25 @@
+package classesPackage;
+
+public class tarefaMod8 {
+
+	public static void main (String args[]) {
+		calculandoMedia();
+		
+	}
+
+	private static void calculandoMedia() {
+		System.out.println("=============== Calculando Média ==================");
+		
+		int num1 = 34;
+		int num2 = 27;
+		int num3 = 18;
+		int num4 = 40;
+		int num5 = num1 + num2 + num3 + num4;
+		float num6 = num5 / 4f;
+		
+		System.out.println("A média desses valores é igual a: " + num6);
+		
+	}
+}
+
+  
